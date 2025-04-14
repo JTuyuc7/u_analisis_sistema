db293fed00fb3f1bdbaa7341a6d29adc553eab48
Jaime Tuyuc <jaime.tuyuc@telus.com>
2025-04-14 12:53:26 -0600

    feat: test route

---
+++ backend/src/routes/testRoute.ts
@@ -0,0 +1,20 @@
+import { Router } from 'express';
+
+const router = Router();
+
+/**
+ * @swagger
+ * /api/test:
+ *   get:
+ *     summary: Test route
+ *     tags: [Test]
+ *     responses:
+ *       200:
+ *         description: Test route is working
+ */
+router.get('/', (req, res) => {
+  res.status(200).json({ message: 'Test route is working' });
+}
+);
+
+export default router;
